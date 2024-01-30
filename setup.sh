3da39854679c758c2effc52c1a494837be47a584
mixer <mixer@penguin.local>
2024-01-30 15:11:55 -0600

    Move to codeberg

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ echo $(xxd -i src/default.lua) >> src/res.h
 echo $(xxd -i src/doc/help.md) >> src/res.h
 echo $(xxd -i src/doc/api.md) >> src/res.h
 echo "#endif" >> src/res.h
+echo "Finished pre-setup"
