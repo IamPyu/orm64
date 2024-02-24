fd4d0e1f73f9f7b788a443cd56ebc1c676f3be0b
IamPyu <pyucreates@outlook.com>
2024-02-24 15:39:38 -0600

    101th commit! And add prebuilt program into package manager.

--- src/default.lua
+++ src/default.lua
@@ -12,7 +12,9 @@ orm64_options.exit_message = string.format("Goodbye, %s!", currentUser)
 orm64_options.prompt = string.format("%s@orm64> ", currentUser) -- The prompt, its that text that is printed before your cursor.
 
 -- Packages
-orm64_options.packages = {} -- Manage packages by git repo, with the Orm64 Package Manager!
+orm64_options.packages = {
+  snowutil = "https://gitlab.com/IamPyu/snowutil"
+} -- Manage packages by git repo, with the Orm64 Package Manager!
 -- Simply insert a string with the git repo URI with a key name.
 -- ex. {mylibrary = "https://git.example.com/gitLover69/mylibrary"}
 -- A more powerful way to manage packages may be added in the future, or you could make your own package manager!
