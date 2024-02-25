891efe1925b30b6530888b0d245d934df848504c
IamPyu <pyucreates@outlook.com>
2024-02-24 18:15:42 -0600

    Modify ormlibTest.c

--- tests/ormlibTest.c
+++ tests/ormlibTest.c
@@ -3,10 +3,10 @@
 #include <ormlib.h>
 
 int main(int argc, const char **argv) {
-    if (argc >= 3) {
-        orm64_createuser(argv[1], argv[2]);
-        printf("Created user: %s; with password: %s\n", argv[1], argv[2]);
-    } else {
-        printf("ormlibTest requires 2 parameters.\n");
-    }
+  if (argc >= 3) {
+    orm64_createuser(argv[1], argv[2]);
+    printf("Created user: %s; with password: %s\n", argv[1], argv[2]);
+  } else {
+    printf("ormlibTest requires 2 parameters.\n");
+  }
 }
