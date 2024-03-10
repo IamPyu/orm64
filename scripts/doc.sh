33e780a6ac2f56972a364d7fb69228e451b6b14f
IamPyu <pyucreates@outlook.com>
2024-03-09 20:29:43 -0600

    Man page generation

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
-mandir=${MESON_INSTALL_PREFIX}/share/man
-mkdir -p "${mandir}"
+mandir="man/man"
+mkdir -p "${MESON_SOURCE_ROOT}/${mandir}"
 
 doc(){
-	out="${MESON_SOURCE_ROOT}/res/${1}.1"
-	pandoc "${MESON_SOURCE_ROOT}/res/${1}.md" -o $out
-	gunzip $out
-
-	if [[ $? -eq 0 ]]; then
-		mv "${MESON_SOURCE_ROOT}/res/${1}.1" $mandir
-		rm -rf $out
-	else
-		mv $out $mandir
-	fi
+	pandoc "${MESON_SOURCE_ROOT}/man/${1}.md" -o "${MESON_SOURCE_ROOT}/${mandir}/${1}.1"
 }
 
 doc orm64
-
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DATADIR=$HOME/.config/orm64
-mkdir -p $DATADIR
-cp -r $MESON_SOURCE_ROOT/default/* $DATADIR
-
-echo "" > /dev/null 
