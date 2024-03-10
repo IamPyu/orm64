a07610064de375a7d85258b328a51747497a19b3
IamPyu <pyucreates@outlook.com>
2024-03-09 20:15:22 -0600

    Document

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,10 +1,18 @@
+#!/bin/sh
 mandir=${MESON_INSTALL_PREFIX}/share/man
 mkdir -p "${mandir}"
 
 doc(){
 	out="${MESON_SOURCE_ROOT}/res/${1}.1"
 	pandoc "${MESON_SOURCE_ROOT}/res/${1}.md" -o $out
-	mv $out $mandir
+	gunzip $out
+
+	if [[ $? -eq 0 ]]; then
+		mv "${MESON_SOURCE_ROOT}/res/${1}.1" $mandir
+		rm -rf $out
+	else
+		mv $out $mandir
+	fi
 }
 
 doc orm64
