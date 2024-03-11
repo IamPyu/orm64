81e4729991f1fddb57229a7919fc9d1d4294f644
IamPyu <pyucreates@outlook.com>
2024-03-10 20:28:58 -0600

    2000th line of code!Orm64 is my biggest project :)

--- default/plugins/myPlugin.c
+++ default/plugins/myPlugin.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-void orm64Setup_myPlugin() {
-  printf("Hello, world!");
+void orm64SetupMyPlugin() {
+  for (int i = 0; i < 5; i++) {
+    printf("Hello, world %d!\n", (i + 1));
+  }
+
+  printf("Orm64 Rocks!\n");
+
+  printf("Enter anything you want (Max 256 characters): ");
+  char str[256];
+  fgets(str, sizeof(str), stdin);
+  str[strlen(str)-1] = '\0';
+
+  printf("You entered:\n%s\n", str);
 }
