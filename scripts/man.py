3d0ab83683316b7e8184045ecea24b955db334fd
IamPyu <pyucreates@outlook.com>
2024-03-23 15:03:25 -0600

    Small changes

--- scripts/man.py
+++ scripts/man.py
@@ -6,13 +6,12 @@ root = os.getenv("MESON_SOURCE_ROOT")
 prefix = os.getenv("MESON_INSTALL_PREFIX")
 
 def setup_man(n):
-    print(f"Root: {root}")
     os.system(f"mkdir -p {prefix}/share/man/man{n}")
     for p in pathlib.Path(root+"/man").glob(f"*.{n}"):
         os.system(f"gzip -fk {p}")
         os.system(f"mv {p.__str__()}.gz {prefix}/share/man/man{n}")
     return 0
 
-# See man(1) for information on how man pages are sorted.
+# See man(1) for information on how man pages are sorted. See groff_man(7) to learn how to write manpages.
 setup_man("1") # General commands
 setup_man("3") # Library functions
