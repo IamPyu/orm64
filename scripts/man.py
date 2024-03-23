4a1ca7013c258b2be2622f022a3ac6bbcf74a802
IamPyu <pyucreates@outlook.com>
2024-03-23 14:54:58 -0600

    Fix manpage script

--- scripts/man.py
+++ scripts/man.py
@@ -6,9 +6,11 @@ root = os.getenv("MESON_SOURCE_ROOT")
 prefix = os.getenv("MESON_INSTALL_PREFIX")
 
 def setup_man(n):
+    print(f"Root: {root}")
     os.system(f"mkdir -p {prefix}/share/man/man{n}")
-    os.system(f"gzip {root}/man/*.{n}")
-    for p in pathlib.Path(root+"/man").glob(f"*.{n}.gz"): os.system(f"mv {p.__str__()} {prefix}/share/man/man{n}")
+    for p in pathlib.Path(root+"/man").glob(f"*.{n}"):
+        os.system(f"gzip -fk {p}")
+        os.system(f"mv {p.__str__()}.gz {prefix}/share/man/man{n}")
     return 0
 
 # See man(1) for information on how man pages are sorted.
